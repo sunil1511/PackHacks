19fb1be7611e51d4fc3a74412beda0da9e6f7b8c
Sumedh Salvi <salvisumedh2396@gmail.com>
2022-04-02 21:58:46 -0400

    Added index files for Checkboardio

---
+++ Jeopardy/templates/js/main.js
@@ -0,0 +1,91 @@
+import questions from './questions.js';
+
+// Buzzer & Chaching sounds
+var buzzer = document.getElementById("buzzer");
+buzzer = window.buzzer;
+function playbuzzer() {
+    buzzer.play();
+}
+function pausebuzzer() {
+    buzzer.pause();
+}
+var chaching = document.getElementById("chaching");
+chaching = window.chaching;
+function playchaching() {
+    chaching.play();
+}
+function pausechaching() {
+    chaching.pause();
+}
+// Global variable  score...
+var score = 0;
+//
+$(function() {
+   // Hide the Modal after submit
+    $("#closesubmit").click(function(){
+    $("#myModal").modal("hide");
+    });
+});
+$(function() {
+  // Remove Element after click
+  $(".gridbtn").click(function() {
+    $(this).addClass("disabled");
+    $(this).remove();
+    });
+});
+ // Submit Question Answer
+function submit(a, b) {
+  var selected = $(".modal-body input:checked").val();
+  // Determine if correct answer was chosen and alert prize
+  if ( selected === questions[window.currentQuestion].correctAnswer ) {
+    // console.log(selected + " was selected");
+    chaching.play();
+    window.addprize = questions[window.currentQuestion].cashPrize;
+    score = score + window.addprize;
+    console.log(score);
+    $('#score1').html("  $"+score);
+  }
+  // Determine if incorrect answer was chosen and alert prize
+  if ( selected !== questions[window.currentQuestion].correctAnswer ) {
+    buzzer.play();
+    window.subprize = questions[window.currentQuestion].cashPrize;
+    score = score - window.subprize;
+    $('#score1').html("  $"+score);
+  }
+}
+// Get question info from array, prepare
+function getOptions(question) {
+  var $buttonDiv = $('<div id="disabled" class="btn-group" data-toggle="buttons"></div>');
+  question.options.forEach(function(opt) {
+    var $div = $('<div class="radio">');
+    var $label = $('<label class="radio-inline"></label');
+    var $input = $('<input type="radio" name="opts" value="' + opt + '">');
+    $label.append($input);
+    $label.append(opt);
+    $div.append($label);
+    $buttonDiv.append($div);
+  });
+  return $buttonDiv;
+}
+// Populate modal window with specific question
+function showQuestion(event, $modal) {
+  var button = $(event.relatedTarget);  // Button that triggered the modal
+  var num = parseInt(button.data('num'));
+  var question = questions[num];
+  window.currentQuestion = num;
+  $modal.find('.modal-title').text(question.prompt);
+  $modal.find('.modal-body').empty().append(getOptions(question));
+}
+// Modal show/close functions
+$(function() {
+  $("#myModal").on('show.bs.modal', function(event) {
+    showQuestion(event, $(this));
+  });
+});
+$("#myModal").on('hidden.bs.modal', function () {
+            console.log('The modal is now hidden.');
+    });
+ $('.modal-body input:checked').submit(function() {
+    $('#myModal').modal('toggle');
+    return false;
+});
